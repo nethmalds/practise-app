44467797542b3dcb3e72f37ecf56b4e08898b180
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-05-28 12:00:20 +0530

    refactor: change the vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on mode (development, production, etc.)
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-  base: env.VITE_APP_DEV === 'true' ? "/" : "/practise-app/",
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
@@ -18,6 +13,5 @@ export default defineConfig(({ mode }) => {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
-  };
+  },
 })
