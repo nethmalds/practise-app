191bba946f0ada7855bffebeb3147209262d8a86
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-05-28 11:54:46 +0530

    feat: initializing the deployment workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+  base: env.VITE_APP_DEV === 'true' ? "/" : "/practise-app/",
   plugins: [
     react(),
     tailwindcss(),
@@ -13,5 +18,6 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
+  };
 })
